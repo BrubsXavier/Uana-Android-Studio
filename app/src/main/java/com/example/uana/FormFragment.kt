3225d907da33844a9b3d5430b2e7dbd0bdb29e90
Bruna Xavier <brunavxr@gmail.com>
2022-10-04 17:03:50 -0300

    Hilt e Retrofit

--- app/src/main/java/com/example/uana/FormFragment.kt
+++ app/src/main/java/com/example/uana/FormFragment.kt
@@ -2,14 +2,19 @@ package com.example.uana
 
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.uana.databinding.FragmentFormBinding
+import com.example.uana.model.Categoria
 import com.example.uana.model.Produto
 
 
@@ -17,7 +22,7 @@ class FormFragment : Fragment() {
 
     private lateinit var binding: FragmentFormBinding
     private val mainViewModel: MainViewModel by activityViewModels()
-
+    private var categoriaSelecionada = 0L
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -28,8 +33,15 @@ class FormFragment : Fragment() {
 
         binding = FragmentFormBinding.inflate(layoutInflater, container, false)
 
+        mainViewModel.listCategoria()
+
+        mainViewModel.myCategoriaResponse.observe(viewLifecycleOwner) { response ->
+            Log.d("Requisicao", response.body().toString())
+            spinnerCategoria(response.body())
+        }
+
         binding.buttonSalvar.setOnClickListener {
-            inserirNaLista()
+            inserirNoBanco()
         }
 
         binding.buttonCancelar.setOnClickListener {
@@ -39,6 +51,7 @@ class FormFragment : Fragment() {
 
         return binding.root
     }
+
 
     private fun validarCampos(
         nome: String,
@@ -57,16 +70,19 @@ class FormFragment : Fragment() {
 
     }
 
-    private fun inserirNaLista() {
+    private fun inserirNoBanco() {
 
-        val nome = binding.nome.text.toString()
-        val desc = binding.descricao.text.toString()
-        val price = binding.preco.text.toString()
-        val imagem = binding.imagemProduto.text.toString()
-        val estoq = binding.estoque.text.toString().toInt()
-
+        val nome = binding.editNome.text.toString()
+        val desc = binding.editDescricao.text.toString()
+        val price = binding.editPreco.text.toString()
+        val imagem = binding.editImagemProduto.text.toString()
+        val estoq = binding.editEstoque.text.toString().toInt()
+        val categoria = Categoria(categoriaSelecionada, " ", null)
 
         if (validarCampos(nome, desc, price, imagem, estoq)) {
+
+            val produto = Produto(0, nome, desc, 0, price, imagem, 0, categoria)
+            mainViewModel.addProduto(produto)
 
             Toast.makeText(context, "Produto criado", Toast.LENGTH_SHORT).show()
 
@@ -79,5 +95,29 @@ class FormFragment : Fragment() {
         }
     }
 
+    private fun spinnerCategoria(listCategoria: List<Categoria>?) {
+
+        if (listCategoria != null) {
+            binding.spinnerCategoria.adapter =
+                ArrayAdapter(
+                    requireContext(),
+                    androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,
+                    listCategoria
+                )
+            binding.spinnerCategoria.onItemSelectedListener =
+                object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                        val selected = binding.spinnerCategoria.selectedItem as Categoria
+                        categoriaSelecionada = selected.id
+                    }
+
+                    override fun onNothingSelected(p0: AdapterView<*>?) {
+
+                    }
+
+                }
+        }
+
+    }
 
 }
